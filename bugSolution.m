50e9b5be5e9ec9d2471a73c33772ff244f952cb8
BugHunter-X <askzenai@gmail.com>
2025-02-23 21:38:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,21 @@
+function result = myFunction(x)
+  if x > 10
+    result = x^2; 
+  elseif x < 0
+    result = 0; 
+  elseif x == 0
+    result = 0; % Handle the case when x is exactly 0
+  else
+    result = x + 5; 
+  end
+end
+
+%Test cases
+>> myFunction(12)
+ans = 144
+>> myFunction(-2)
+ans = 0
+>> myFunction(5)
+ans = 10
+>> myFunction(0)
+ans = 0
